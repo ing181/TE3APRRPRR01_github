d62f21dafa640b45ff933a3ae355ba0ef19549ee
ing181 <ingcei0123@edu.lerum.se>
2019-09-20 11:50:47 +0200

    Lektion

---
+++ 2019-09-20/main.cpp
@@ -0,0 +1,21 @@
+// Biblioteket iostream ingår
+#include <iostream>
+// Namnrymden std avses
+using namespace std;
+int main() {
+    // datatyp namn;
+	// heltalsvariabler
+	// short, int, long
+	// samt char	
+	int tal;
+	tal = 4;
+	cout << tal;
+	cin >> tal;
+	cout << tal;
+	// << utmatningsoperatorn
+	
+	
+	
+	// Om allt fungerat hit
+	return 0;
+}
