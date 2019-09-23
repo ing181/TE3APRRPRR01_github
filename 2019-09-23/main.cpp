98e514301baa13355b4336fc6f14d8c7904b6a87
Ingemar Ceicer <ingcei0123@lerum.local>
2019-09-23 17:18:16 +0200

    Lektion

---
+++ 2019-09-23/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main() {
+	
+	// "cout" är skärmen, "<<" är utmatningsoperatorn 
+     cout << "HEJ "; // Utsskrift av en textsträng till skärmen
+     // Om variabler
+     // Man skriver alltid
+     // "datatyp namn", en variabel deklareras (skapas)
+     // 
+     // Exempel
+     int tal; // variabel deklareras (skapas)
+     tal = 6; // variabel får ett värde (tilldelas)
+     cout << tal;
+	 return 0; 
+}
