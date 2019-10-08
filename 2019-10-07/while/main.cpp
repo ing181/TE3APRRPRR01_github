2a32d90ea0fe55f34edbabac24e646079e111105
Ingemar Ceicer <ingcei0123@lerum.local>
2019-10-08 11:09:13 +0200

    Lektion

---
+++ 2019-10-07/while/main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+int main() {
+	
+	int i = 0;
+	while (i < 10)
+	{
+		cout << i << " SANT" << endl;
+		i++;
+	}
+	
+	do {
+		
+		cout << "1>2" << endl;
+		
+	} while (1<2);
+	
+	
+	
+
+	return 0;
+}
